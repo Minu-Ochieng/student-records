fabd1128e5ab29c0d333c8b834203973396b6c2d
Casey <minuochieng@gmail.com>
2024-02-08 21:06:01 +0300

    student records

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main(){
+    var name = "Casey Ochieng"
+    name = "Casey Ochieng"
+    println(name)
+    var caseyAge: Byte = 21
+    println(caseyAge)
+    var caseyContact: String = "+254741151470"
+    println(caseyContact)
+    var caseyWeight: Double = 54.3
+    println(caseyWeight)
+    var caseyKenyanStatus: Boolean = true
+    println(caseyKenyanStatus)
+}
